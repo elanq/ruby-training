38722a4ee67a058da654db269e0df214166e7807
Elan Qisthi <elanqisthi@gmail.com>
2016-04-06 11:21:34 +0700

    create pic action and view files

--- instagram-app/app/controllers/pics_controller.rb
+++ instagram-app/app/controllers/pics_controller.rb
@@ -2,5 +2,18 @@ class PicsController < ApplicationController
 
   def index
   end
-  
+
+  def create
+    @pic = Pic.new(pic_params)
+  end
+
+  def new
+    # create new instance of pic
+    @pic = Pic.new
+  end
+
+  private
+    def pic_params
+      params.require(:pic).permit(:title, :description)
+    end
 end
