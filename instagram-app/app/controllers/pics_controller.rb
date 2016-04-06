d087541878a1d719068a5899d608c07719c162dd
Elan Qisthi <elanqisthi@gmail.com>
2016-04-06 11:36:12 +0700

    Create index pic and show

--- instagram-app/app/controllers/pics_controller.rb
+++ instagram-app/app/controllers/pics_controller.rb
@@ -1,10 +1,18 @@
 class PicsController < ApplicationController
+  before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
   def index
+    @pics = Pic.all.order('created_at DESC')
   end
 
   def create
     @pic = Pic.new(pic_params)
+
+    if @pic.save
+      redirect_to @pic, notice: "Pic is saved"
+    else
+      render 'new'
+    end
   end
 
   def new
@@ -12,8 +20,16 @@ class PicsController < ApplicationController
     @pic = Pic.new
   end
 
+  def show
+
+  end
+
   private
     def pic_params
       params.require(:pic).permit(:title, :description)
+    end
+
+    def find_pic
+      @pic = Pic.find(params[:id])
     end
 end
