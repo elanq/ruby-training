13a650d50c701b0887839d7b410d67cd6f60902e
Elan Qisthi <elanqisthi@gmail.com>
2016-04-06 10:27:18 +0700

    Add some restrictions

--- tumblr-app/app/controllers/posts_controller.rb
+++ tumblr-app/app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   def index
-    @posts = Post.all.order('created_at DESC')
+    @posts = Post.all.order('created_at DESC')    
   end
 
   def new
@@ -39,6 +40,7 @@ class PostsController < ApplicationController
   end
 
   private
+
 
     def find_post
       @post = Post.find(params[:id]) 
