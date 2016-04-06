6dfc84b08a4210b1228a59f5cd21f44b7ff1a2b0
Elan Qisthi <elanqisthi@gmail.com>
2016-04-06 12:46:58 +0700

    Add devise and user association with pictures

--- instagram-app/app/controllers/pics_controller.rb
+++ instagram-app/app/controllers/pics_controller.rb
@@ -1,12 +1,13 @@
 class PicsController < ApplicationController
   before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
+
   def index
     @pics = Pic.all.order('created_at DESC')
   end
 
   def create
-    @pic = Pic.new(pic_params)
+    @pic = current_user.pics.build(pic_params)
 
     if @pic.save
       redirect_to @pic, notice: "Pic is saved"
