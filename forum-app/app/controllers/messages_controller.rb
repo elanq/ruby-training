047c0784317f2e8f19c32b9e96dd42c50e2a2ff4
Elan Qisthi <elanqisthi@gmail.com>
2016-04-07 15:49:31 +0700

    new message view

--- forum-app/app/controllers/messages_controller.rb
+++ forum-app/app/controllers/messages_controller.rb
@@ -1,11 +1,21 @@
 class MessagesController < ApplicationController
+  before_action :find_message, only: [:update, :destroy, :show]
+
   def index
   end
 
   def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      redirect_to root_path
+    else
+      render 'new'
+    end 
   end
 
   def new
+    @message = Message.new
   end
 
   def edit
@@ -15,5 +25,15 @@ class MessagesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:title, :description)
+  end
+
+  def find_message
+    @message = Message.find(params(:id))
   end
 end
