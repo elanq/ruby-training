3871919c2e24d3baf0715a5c1213a4653c60c329
Elan Qisthi <elanqisthi@gmail.com>
2016-04-07 17:21:33 +0700

    Added user authentication with devise

--- forum-app/app/controllers/messages_controller.rb
+++ forum-app/app/controllers/messages_controller.rb
@@ -1,13 +1,13 @@
 class MessagesController < ApplicationController
   before_action :find_message, only: [ :destroy, :show, :edit, :update]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @messages = Message.all.order('created_at DESC');
   end
 
   def create
-    @message = Message.new(message_params)
-
+    @message = current_user.messages.build(message_params)  
     if @message.save
       redirect_to root_path
     else
@@ -16,7 +16,7 @@ class MessagesController < ApplicationController
   end
 
   def new
-    @message = Message.new
+    @message = current_user.messages.build
   end
 
   def edit
